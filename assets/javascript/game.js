d9ff098e91c7c972fb8a71db5ccf5cef8b1bf5ff
Adam Kwan <adamk1230@gmail.com>
2017-05-24 08:35:27 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,22 @@
 // Random sports words
 var words = ["football", "basketball", "baseball", "soccer", "tennis", "golf"];
 var randomWord = words[Math.floor(Math.random() * words.length)];
-var randomArray = [];
-randomArray.push(randomWord.split(""));
+// var randomArray = [];
+// randomArray.push(randomWord.split(""));
 
-console.log(randomWord);
-console.log(randomArray);
+		// document.getElementById("letter").value = '';
+
+
+        letters = document.getElementById("letters");
+        letters.innerHTML = "<li class='current-word'>Current word:</li>";
+
+        var letter, i;
+        for (i = 0; i < currentWord.length; i++) {
+            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+            letters.insertAdjacentHTML('beforeend', letter);
+        }
+
+// console.log(randomWord);
+// console.log(randomArray);
 
 // 
