020779804acc626a6061019ec7d3b5af6053a8e9
Adam Kwan <adamkwan@Adams-MacBook.home>
2017-05-24 23:01:07 -0400

    blank spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,66 @@
 
 // Random sports words
 var words = ["football", "basketball", "baseball", "soccer", "tennis", "golf"];
-var randomWord = words[Math.floor(Math.random() * words.length)];
-// var randomArray = [];
-// randomArray.push(randomWord.split(""));
+var selectWord = words[Math.floor(Math.random() * words.length)];
+var splitSelectWord = selectWord.split('')
+var text = "";
+
+console.log(splitSelectWord);
+
+// Print blank spaces
+for (var i = 0; i < splitSelectWord.length; i++) {
+
+	text = text + "_" + " ";
+
+        document.getElementById("letters").innerHTML = text;
+}
+
+
+document.onkeyup = function(event) {
+   var userGuess = event.key;
+
+   if(splitSelectWord.indexOf(userGuess) > -1){
+    var showLetter;
+   }
+
+
+}  //end of doconkeyup
+
+
+// document.getElementById("letters").innerHTML = "<p>This is a test</p>";
+// document.getElementById("letters").innerHTML = text;
+
+
+// var selectArray = [];
+// console.log(selectWord)
+// console.log(selectWord.split(""))
+// var selectArray = selectArray.concat(selectWord.split(""));
+// console.log(selectArray);
+// selectArray.concat(selectWord.split(""));
+// selectArray.push("f");
+// selectArray.push("o");
+// console.log(selectWord);
+// console.log(selectArray);
+
+// console.log(selectArray.length);
+// for (var i = 0; i >= selectArray.length; i++) {
+// 	console.log(selectArray[i]);
+// }	
+
+
 
 		// document.getElementById("letter").value = '';
 
 
-        letters = document.getElementById("letters");
-        letters.innerHTML = "<li class='current-word'>Current word:</li>";
+        // letters = document.getElementById("letters");
+        // letters.innerHTML = "<li class='current-word'>Current word:</li>";
 
-        var letter, i;
-        for (i = 0; i < currentWord.length; i++) {
-            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
-            letters.insertAdjacentHTML('beforeend', letter);
-        }
+        // var letter, i;
+        // for (i = 0; i < currentWord.length; i++) {
+        //     letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+        //     letters.insertAdjacentHTML('beforeend', letter);
+        // }
 
-// console.log(randomWord);
-// console.log(randomArray);
+
 
 // 
